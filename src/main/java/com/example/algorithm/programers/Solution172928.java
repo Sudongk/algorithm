c41643cb619553708db06d3321c0d485eb409563
Sudongk <82927161+Sudongk@users.noreply.github.com>
2023-04-12 10:04:13 +0900

    Programers 172928

--- src/main/java/com/example/algorithm/programers/Solution172928.java
+++ src/main/java/com/example/algorithm/programers/Solution172928.java
@@ -20,19 +20,24 @@ public class Solution172928 {
             int move = Integer.parseInt(command[1]);
             int[] cors = {0, 0};
 
+            // ex) cors = [0,0] -> [0, 1]
             if (direction.equals("E")) {
                 cors[1]++;
+            // ex) cors = [0,0] -> [0, -1]
             } else if (direction.equals("W")) {
                 cors[1]--;
+            // ex) cors = [0,0] -> [1, 0]
             } else if (direction.equals("S")) {
                 cors[0]++;
+            // ex) cors = [0,0] -> [-1, 0]
             } else if (direction.equals("N")) {
                 cors[0]--;
             }
 
             // 맵을 벗어나는지 확인
-            int nX = currentPos[0] + (cors[0] * move);
-            int nY = currentPos[1] + (cors[1] * move);
+            int nX = currentPos[0] + (cors[0] * move); // 0 + (0 * 0) = 0
+            int nY = currentPos[1] + (cors[1] * move); // 0 + (1 * 2) = 2
+
             if (nX < 0 || nX >= park.length || nY < 0 || nY >= park[0].length()) {
                 continue;
             }
@@ -41,6 +46,7 @@ public class Solution172928 {
             boolean meetObstacle = false;
             int tempX = currentPos[0];
             int tempY = currentPos[1];
+
             for (int i = 1; i <= move; i++) {
                 tempX += cors[0];
                 tempY += cors[1];
@@ -54,7 +60,7 @@ public class Solution172928 {
                 continue;
             }
 
-            // 문제가 없으면 강아지가 이동
+            // 문제가 없으면 이동
             currentPos[0] = nX;
             currentPos[1] = nY;
         }
