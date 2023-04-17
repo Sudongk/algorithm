aa2222058f0c9a2f4fa68a80dd1e56b010e90dac
Sudongk <82927161+Sudongk@users.noreply.github.com>
2023-04-17 23:51:36 +0900

    Programers 160586

--- src/main/java/com/example/algorithm/programers/Solution160586.java
+++ src/main/java/com/example/algorithm/programers/Solution160586.java
@@ -1,5 +1,8 @@
 package com.example.algorithm.programers;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Solution160586 {
     // keymap : ["ABACD", "BCEFD"]
     // targets :["ABCD","AABB"]
@@ -7,46 +10,49 @@ public class Solution160586 {
     public int[] solution(String[] keymap, String[] targets) {
         int[] answer = new int[targets.length];
 
-        for (int i = 0; i < targets.length; i++) {
-            int sum = 0;
-            String[] target = targets[i].split("");
-            for (int j = 0; j < target.length; j++) {
-                int idx = checkExist(keymap, target[j]);
-                if (idx != 1000000) {
-                    sum += idx + 1;
-                } else {
-                    sum = idx;
+        // keymap의 각 요소의 인덱스를 Map형태로 저장
+        HashMap<Character,Integer> map = new HashMap<>();
+
+        for (String key : keymap) {
+            for (int j = 0; j < key.length(); j++) {
+
+                char curIdx = key.charAt(j);
+
+                if (map.containsKey(curIdx)){
+                    // 현재 인덱스가 기존의 인덱스보다 작으면 교체
+                    if(map.get(curIdx) > j){
+                        map.replace(curIdx, j);
+                    }
+                }else{
+                    map.put(curIdx, j);
                 }
             }
-
-            if (sum == 1000000) {
-                sum = -1;
-            }
-
-            answer[i] = sum;
         }
 
+        for(int i = 0; i < targets.length; i++){
+            int sum = 0;
+
+            for(int j=0; j<targets[i].length();j++){
+
+                char target = targets[i].charAt(j);
+
+                if(map.containsKey(target)){
+                    // 인덱스이기 때문에 실체 누른 횟수 출력을 위한 +1
+                    sum += map.get(target) + 1;
+                }else{
+                    sum = -1;
+                    break;
+                }
+            }
+            answer[i] = sum;
+        }
 
         return answer;
     }
 
-    public static int checkExist(String[] keymap, String target) {
-        int index = 1000000;
-        for (int i = 0; i < keymap.length; i++) {
-            if (keymap[i].contains(target)) {
-                int newIndex = keymap[i].indexOf(target);
-                if (newIndex < index) {
-                    index = newIndex;
-                }
-            }
-        }
-
-        return index;
-    }
-
     public static void main(String[] args) {
-        String[] keymap = {"AA", "AWWER", "AAWC"};
-        String[] targets = {"B", "WW", "B"};
+        String[] keymap = {"NAA", "PPAWWER", "QAAWC"};
+        String[] targets = {"AAAAA", "WW", "B"};
         Solution160586 solution = new Solution160586();
         int[] result = solution.solution(keymap, targets);
 
