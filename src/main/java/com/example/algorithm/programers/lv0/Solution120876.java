b932bd6daad3d24c0244460a92530aa4a8f0826f
Sudongk <82927161+Sudongk@users.noreply.github.com>
2023-05-20 09:57:03 +0900

    Programers Solution 120876

---
+++ src/main/java/com/example/algorithm/programers/lv0/Solution120876.java
@@ -0,0 +1,11 @@
+package com.example.algorithm.programers.lv0;
+
+public class Solution120876 {
+    // lines : [[0, 1], [2, 5], [3, 9]]
+    // result : 2
+    public int solution(int[][] lines) {
+        int answer = 0;
+
+        return answer;
+    }
+}
