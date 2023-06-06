6cde3f4840c1af5ab0bed36d58c66a6dc794f083
김수동 <kimsudong@gimsudong-ui-MacBookPro.local>
2023-06-06 23:06:37 +0900

    Programmers Solution 181893

--- src/main/java/com/example/algorithm/programers/lv0/Solution181893.java
+++ src/main/java/com/example/algorithm/programers/lv0/Solution181893.java
@@ -11,7 +11,7 @@ public class Solution181893 {
 
         int start = 0, end = 0;
 
-        // 실제 배열과 비교하며 인덱스 조정
+        // 실제 배열과 비교하며 인덱스 조정하여 범위값 구하기
         for (int i = 0; i < query.length; i++) {
             if (i % 2 != 0) {
                 start = start + query[i];
