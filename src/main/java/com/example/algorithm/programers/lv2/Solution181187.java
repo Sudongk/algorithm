8f03dc7611f517c9a1a726ab680520bc8c03176a
Sudongk <82927161+Sudongk@users.noreply.github.com>
2023-06-25 19:27:04 +0900

    Programmers Solution 181187

---
+++ src/main/java/com/example/algorithm/programers/lv2/Solution181187.java
@@ -0,0 +1,11 @@
+package com.example.algorithm.programers.lv2;
+
+public class Solution181187 {
+    // r1 : 2
+    // r2 : 3
+    // result : 20
+    public long solution(int r1, int r2) {
+        long answer = 0;
+        return answer;
+    }
+}
