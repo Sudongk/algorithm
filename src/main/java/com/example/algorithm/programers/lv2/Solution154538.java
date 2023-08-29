76e11eed28651e5de336d140790e2e519d14095d
Sudongk <82927161+Sudongk@users.noreply.github.com>
2023-08-29 23:25:25 +0900

    Programers Solution 154538

--- src/main/java/com/example/algorithm/programers/lv2/Solution154538.java
+++ src/main/java/com/example/algorithm/programers/lv2/Solution154538.java
@@ -6,14 +6,26 @@ public class Solution154538 {
     // n : 5
     // result : 2
     public int solution(int x, int y, int n) {
-        int answer = 0;
-
-        
-
-        return answer;
+        int cnt = 0;
+        while (y > x) {
+            if (y % 2 == 0 && y / 2 >= x) {
+                y /= 2;
+            } else if (y % 3 == 0 && y / 3 >= x && (y / 3) % n != n - 1) {
+                y /= 3;
+            } else {
+                y -= n;
+            }
+            cnt++;
+        }
+        return x == y ? cnt : -1;
     }
 
     public static void main(String[] args) {
+        int x = 10;
+        int y = 40;
+        int n = 5;
 
+        Solution154538 solution154538 = new Solution154538();
+        System.out.println(solution154538.solution(x, y, n));
     }
 }
