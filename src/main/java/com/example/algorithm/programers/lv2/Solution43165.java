bdbf7eaacffb086b94d8cb67a752b437928ba005
Sudongk <82927161+Sudongk@users.noreply.github.com>
2023-09-23 16:43:27 +0900

    Programmers Solution 43165

--- src/main/java/com/example/algorithm/programers/lv2/Solution43165.java
+++ src/main/java/com/example/algorithm/programers/lv2/Solution43165.java
@@ -4,12 +4,31 @@ public class Solution43165 {
     // numbers : [1, 1, 1, 1, 1]
     // target : 3
     // result : 5
+    private static int cnt = 0;
+
     public int solution(int[] numbers, int target) {
-        int answer = 0;
-
-
+        dfs(0, target, numbers);	// DFS를 이용해 풀이
+        int answer = cnt;
 
         return answer;
+    }
+
+    public void dfs(int index, int target, int[] numbers){
+        if(index == numbers.length){	// 주어진 배열의 마지막 자리일 경우
+            int sum = 0;				// sum초기화
+            for(int i=0; i<numbers.length; i++){
+                sum += numbers[i];		// sum에 주어진 배열의 수를 더한다
+            }
+            if(sum == target){			// 배열을 모두 더한 값이 target과 같은 경우
+                cnt++;					// cnt++
+            }
+        } else {						// 마지막자리가 아닐 경우 재귀함수를 통해 부호를 바꿔가며 탐색
+            numbers[index] *= 1;
+            dfs(index+1, target, numbers);
+
+            numbers[index] *= -1;
+            dfs(index+1, target, numbers);
+        }
     }
 
     public static void main(String[] args) {
