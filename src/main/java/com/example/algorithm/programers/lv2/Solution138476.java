e3d72b4744866a6fce3991aca2b15fd29fadcc6c
Sudongk <aa88sd@naver.com>
2023-10-01 22:27:55 +0900

    Programmers Solution 138476

--- src/main/java/com/example/algorithm/programers/lv2/Solution138476.java
+++ src/main/java/com/example/algorithm/programers/lv2/Solution138476.java
@@ -1,6 +1,6 @@
 package com.example.algorithm.programers.lv2;
 
-import java.util.Arrays;
+import java.util.*;
 
 public class Solution138476 {
     // k : 6
@@ -9,14 +9,49 @@ public class Solution138476 {
     public int solution(int k, int[] tangerine) {
         int answer = 0;
 
-        int[] sortedTangerine = Arrays.stream(tangerine).sorted().toArray();
+        Map<Integer, Integer> map = new HashMap<>();
 
+        for (int e : tangerine) {
+            if (!map.containsKey(e)) {
+                map.put(e, 1);
+                continue;
+            }
 
+            map.put(e, map.get(e) + 1);
+        }
+
+        // Map의 value 값만 추출하여 리스트로 저장
+        List<Integer> values = new ArrayList<>(map.values());
+
+        // value 값으로 내림차순 정렬
+        values.sort(Collections.reverseOrder());
+
+        int count = 0;
+
+        for (Integer value : values) {
+            if (count == 0 && value == k) {
+                answer++;
+                break;
+            }
+
+            if (count < k) {
+                count += value;
+                answer++;
+            }
+
+            if (count == k && value > k) {
+                break;
+            }
+        }
 
         return answer;
     }
 
     public static void main(String[] args) {
+        int k = 6;
+        int[] tangerine = {1, 3, 2, 5, 4, 5, 2, 3};
 
+        Solution138476 solution138476 = new Solution138476();
+        System.out.println(solution138476.solution(k, tangerine));
     }
 }
