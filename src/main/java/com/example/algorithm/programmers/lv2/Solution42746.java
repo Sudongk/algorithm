bf8e5ccb896389e82da8212f827a5da347a47e3d
Sudongk <aa88sd@naver.com>
2024-04-09 23:26:20 +0900

    Programmers Solution 42746 시간초과 문제 해결

--- src/main/java/com/example/algorithm/programmers/lv2/Solution42746.java
+++ src/main/java/com/example/algorithm/programmers/lv2/Solution42746.java
@@ -8,45 +8,33 @@ public class Solution42746 {
     // return : "6210"
 
     public String solution(int[] numbers) {
-        String answer = "";
-
-        // 우선순위 큐 - 최대 힙(내림차순)
-        PriorityQueue<Integer> pq = new PriorityQueue<>(((o1, o2) -> o2 - o1));
-
-        // 조합 가능한 숫자 구하고 우선순위 큐에 삽입
-        permutation(numbers, new int[numbers.length], new boolean[numbers.length], 0, numbers.length, pq);
-
-        answer = String.valueOf(pq.poll());
-
-        return answer;
-    }
-
-    // 순열을 구하는 재귀 함수
-    public void permutation(int[] numbers, int[] out, boolean[] visited, int depth, int r, PriorityQueue<Integer> pq) {
-        StringBuilder sb = new StringBuilder();
-        if (depth == r) {
-            for (int i = 0; i < r; i++) {
-                if (out[i] == 0) {
-                    break;
-                }
-                sb.append(out[i]);
-            }
-            pq.add(Integer.parseInt(sb.toString()));
-            sb.setLength(0);
-        }
-
+        // Integer 배열을 String 배열로 변환
+        String[] nums = new String[numbers.length];
         for (int i = 0; i < numbers.length; i++) {
-            if (!visited[i]) {
-                visited[i] = true;
-                out[depth] = numbers[i];
-                permutation(numbers, out, visited, depth + 1, r, pq);
-                visited[i] = false;
-            }
+            nums[i] = String.valueOf(numbers[i]);
         }
+
+        // 정렬 기준 설정
+        Arrays.sort(nums, (a, b) -> (b + a).compareTo(a + b));
+
+        System.out.println(Arrays.toString(nums));
+
+        // 모든 수가 0인 경우 예외 처리
+        if (nums[0].equals("0")) {
+            return "0";
+        }
+
+        // 정렬된 문자열을 이어붙여 결과 반환
+        StringBuilder sb = new StringBuilder();
+        for (String num : nums) {
+            sb.append(num);
+        }
+
+        return sb.toString();
     }
 
     public static void main(String[] args) {
-        int[] numbers = {3, 30, 34, 5, 9};
+        int[] numbers = {3, 30, 34, 5, 9, 3, 30, 34, 5, 9,34, 5, 934, 5, 9};
 
         Solution42746 solution = new Solution42746();
 
