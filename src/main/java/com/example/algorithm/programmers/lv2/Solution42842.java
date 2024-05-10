d52c97dbdbef429427cd8068a59375d21a26a54c
Sudongk <aa88sd@naver.com>
2024-05-10 11:39:27 +0900

    Programmers Solution 42842 수정

--- src/main/java/com/example/algorithm/programmers/lv2/Solution42842.java
+++ src/main/java/com/example/algorithm/programmers/lv2/Solution42842.java
@@ -8,17 +8,33 @@ public class Solution42842 {
     public int[] solution(int brown, int yellow) {
         int[] answer = new int[2];
 
+        // 전체 사이즈
         int size = brown + yellow;
 
         int width = 0;
         int length = 0;
 
+        // 가로, 세로 길이 구하기
         for (int i = 1; i <= size; i++) {
+            // 약수인 경우
             if (size % i == 0) {
+                // size를 i로 나눈 값이 세로 길이
+                int newLength = size / i;
+                // size를 i로 나눈 나머지가 가로 길이
+                int newWidth = i % size;
+
+                int yellowSize = (newWidth - 2) * (newLength - 2);
+
+                // 노란색 카펫의 사이즈와 일치하지 않는 경우
+                if (yellowSize != yellow) {
+                    continue;
+                }
+
                 length = size / i;
                 width = i % size;
             }
 
+            // 가로가 세로보다 크거나 같은 경우
             if (length != 0 && width != 0) {
                 if (length <= width) {
                     break;
@@ -33,8 +49,8 @@ public class Solution42842 {
     }
 
     public static void main(String[] args) {
-        int brown = 24;
-        int yellow = 24;
+        int brown = 18;
+        int yellow = 6;
 
         Solution42842 solution = new Solution42842();
 
