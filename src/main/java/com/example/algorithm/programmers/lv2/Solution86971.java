f6a3dcf9acb5f022086805635c942d782b88d1a1
Sudongk <aa88sd@naver.com>
2024-05-13 22:23:08 +0900

    Programmers Solution 86971

--- src/main/java/com/example/algorithm/programmers/lv2/Solution86971.java
+++ src/main/java/com/example/algorithm/programmers/lv2/Solution86971.java
@@ -8,67 +8,77 @@ public class Solution86971 {
     // wires : [[1,3],[2,3],[3,4],[4,5],[4,6],[4,7],[7,8],[7,9]]
     // return : 3
 
+    // 반례
+    // n :9
+    // wires: [[1,2],[2,3],[3,4],[4,5],[5,6],[6,7],[7,8],[8,9]]
+    // return : 1
+
     public int solution(int n, int[][] wires) {
         int answer = 999999999;
 
-        Map<Integer, Set<Integer>> map = new HashMap<>();
-
-        for (int i = 1; i <= n; i++) {
-            map.put(i, new HashSet<>());
+        // 0번 인덱스는 사용하지 않음
+        List<List<Integer>> graph = new ArrayList<>();
+        for (int i = 0; i <= n; i++) {
+            graph.add(new ArrayList<>());
         }
 
+        // 인접 리스트 생성
+        for (int[] wire : wires) {
+            graph.get(wire[0]).add(wire[1]);
+            graph.get(wire[1]).add(wire[0]);
+            System.out.println(graph);
+        }
+
+        // 각 간선을 제거하고 bfs로 탐색
         for (int[] wire : wires) {
             int v1 = wire[0];
             int v2 = wire[1];
 
-            Set<Integer> set1 = map.get(v1);
-            Set<Integer> set2 = map.get(v2);
+            // 간선 제거
+            graph.get(v1).remove(Integer.valueOf(v2));
+            graph.get(v2).remove(Integer.valueOf(v1));
 
-//            set1.add(v1);
-            set1.add(v2);
-            set2.add(v1);
-//            set2.add(v2);
+            int aCount = bfs(graph, v1, n);
+            int bCount = bfs(graph, v2, n);
 
-            map.put(v1, set1);
-            map.put(v2, set2);
-        }
+            int diff = Math.abs(aCount - bCount);
 
-        System.out.println(map);
+            answer = Math.min(answer, diff);
 
-        for (int[] wire : wires) {
-            int count1 = countConnectedTower(map, wire[0], wire[1]);
-            int count2 = countConnectedTower(map, wire[1], wire[0]);;
-
-            int diffCount = Math.abs(count1 - count2);
-
-            if (diffCount < answer) {
-                answer = diffCount;
-            }
+            // 제거한 간선 복구
+            graph.get(v1).add(v2);
+            graph.get(v2).add(v1);
         }
 
         return answer;
     }
 
-    public int countConnectedTower(Map<Integer, Set<Integer>> map, int targetNode, int excludeNode) {
-        Set<Integer> connectNode = new HashSet<>();
-        Set<Integer> nodes = map.get(targetNode);
+    private int bfs(List<List<Integer>> graph, int start, int n) {
+        Queue<Integer> queue = new LinkedList<>();
+        boolean[] visited = new boolean[n + 1];
 
-        for (int key : nodes) {
-            if (key == excludeNode) {
-                continue;
+        queue.offer(start);
+        visited[start] = true;
+
+        int count = 1;
+
+        while (!queue.isEmpty()) {
+            int current = queue.poll();
+
+            // 현재 노드와 연결된 노드 탐색
+            for (int next : graph.get(current)) {
+                if (!visited[next]) {
+                    queue.offer(next);
+                    visited[next] = true;
+                    count++;
+                }
             }
-
-            connectNode.addAll(map.get(key));
         }
 
-        return connectNode.size() + 1;
+        return count;
     }
 
-
     public static void main(String[] args) {
-        // 반례
-        // 9, [[1,2],[2,3],[3,4],[4,5],[5,6],[6,7],[7,8],[8,9]]
-        // 1
         int n = 9;
         int[][] wires = {{1, 2}, {2, 3}, {3, 4}, {4, 5}, {5, 6}, {6, 7}, {7, 8}, {8, 9}};
 
